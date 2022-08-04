c88680fdb09582b340812dd3a71bfc66135d271d
yanhackcode15 <iamyanhu@gmail.com>
2022-08-04 15:12:26 -0700

    refactor: created a quiz component to handle clicks locally, can only choose one answer - picking one will unclick rest answers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,123 @@
-import React from "react"
+import React, { useCallback } from "react"
 import ReactDOM from "react-dom"
+// import { nanoid } from 'nanoid'
 import Quiz from "./components/quiz"
-import { nanoid } from 'nanoid'
+
+// (x) render each answer array with color flags when check answer state is true 
+//finish the check answers button toggle to retake quiz, finish logic
+// retake quiz will refetch api, consider updating useEffect? or driven by a button click?
 
 
 
 export default function App() {
 //click on button, render another component
-    const [styles, setStyles] = React.useState({})
+    const overlayDefaultStyles = {
+        backgroundColor: "white",
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        zIndex: "0"
+    }
+    const quizzesDefaultStyles={
+        zIndex: "-1"
+    }
+    const [overlayStyles, setOverlayStyles] = React.useState(overlayDefaultStyles)
+    const [quizzesStyles, setQuizzesStyles] = React.useState(quizzesDefaultStyles)
     const [quizzes, setQuiz]= React.useState([])
+    //quizzes = [ ...{ question: "", answers: [ ...{data:"" , isCorrect: true/flase, clicked: true/false}]}]
+    const [graded, setGraded] = React.useState(false)
+
 
     function hideOverlay(){
-        setStyles({display: "none"})
+        setOverlayStyles(defaultStyles=>({...defaultStyles, display: "none", zIndex: "-1"}))
+        setQuizzesStyles(defaultStyles=>({...defaultStyles, zIndex: "1"}))
     }
-    function randomAnswers(incorrectAnswers, correctAnswer) {
+    function randomSort(incorrectAnswers, correctAnswer) {
+        //randomly sort answers and add two attributes to each answer: clicked, isCorrect
         const pos = Math.round(Math.random()*3)
-        const cp_array=[...incorrectAnswers]
-        cp_array.splice(pos,0, correctAnswer)
+        const cp_array=[...incorrectAnswers].map(i=>({data:i, isCorrect: false, clicked: false}))
+        cp_array.splice(pos,0, {data:correctAnswer, isCorrect: true, clicked: false})
         return cp_array
     }
+
+    function toggleButton(){
+        //will iterate through the entire quiz array, compare answer.clicked clicked and answer.isCorrect, if match, show answer green; otherwise, show answer red and correct anwer green
+        setGraded(prev=>!prev)
+        
+    }
+    const handleAnswerClick = React.useCallback(
+        // function answerChosen(quiz_index, answer_index){
+        (quiz_index, answer_index) => {
+            //the answer with that answer index for the quiz with that index is clicked and change the answer to green
+            //go to i, j in the quiz array and answer , match found, toggle the clicked property
+
+            setQuiz(prevQuizzes=>{
+                const newState = [...prevQuizzes];
+               //set all answers to clicked: false
+                const answers = newState[quiz_index].answers.map((a)=>({...a, clicked: false}))
+                newState[quiz_index].answers = answers;
+                
+                //set new answer to be clicked: true
+                const answer = {
+                    ...newState[quiz_index].answers[answer_index]
+                };
+                answer.clicked = !answer.clicked;
+                newState[quiz_index].answers[answer_index] = answer;
+                console.log(newState[quiz_index])
+
+                return newState;
+            })
+        }, [setQuiz]);
+
+    // function setAnswerStyles(clicked, correct, graded){
+    //     const defaultStyles = {color: "black"}
+    //     if(graded) {
+
+                        
+    //         if (clicked&&correct) {
+    //             return {...defaultStyles, color:"green"}
+    //         }
+    //         else if(clicked){
+    //             return {...defaultStyles, color: "red"}
+    //         }
+    //         else if (correct){
+    //             return {...defaultStyles, color: "green"}
+
+    //         }
+    //         else {
+    //             return {...defaultStyles, color: "black"}
+    //         }
+    //     }
+    //     else {
+    //         return clicked?{...defaultStyles, color:"purple"}:{...defaultStyles, color:"black"}
+    //     }
+    // }
+
+    // function renderedQuizzes(quizArray){
+    //     return quizArray.map((quiz, i)=>{
+    //         const temp = (
+    //         <div key={`q-${i}`}>
+    //             <h2>{quiz.question}</h2>
+    //             <ul>
+    //                 {
+    //                 quiz.answers.map((answer, j)=>{
+                        
+    //                     return (
+    //                     <li 
+    //                         key={`q-${i}-a-${j}`} 
+    //                         onClick={()=>{answerChosen(i,j)}}
+    //                         style={setAnswerStyles(answer.clicked, answer.isCorrect, graded)}
+    //                         >
+    //                         {answer.data}
+    //                     </li>)}) 
+    //                 }
+    //             </ul>
+    //         </div>
+    //         )
+    //         return temp
+    //     })
+    // }
+
     React.useEffect(()=>{
         fetch('https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple')
         .then( response=>{
@@ -28,19 +127,10 @@ export default function App() {
             throw response
         })
         .then(data=>{
-            const quizData = data.results.map(quiz => {return {question: quiz.question, answers: randomAnswers(quiz.incorrect_answers, quiz.correct_answer)}
-                
+            const quizData = data.results.map(quiz => {return {question: quiz.question, answers: randomSort(quiz.incorrect_answers, quiz.correct_answer)}
+                //[...{question: "", answers: [...{data:"" , isCorrect: true/flase, clicked: true/false}]}]
             });
-            const rendered_quizzes=quizData.map(quiz=>{return(
-                <div key={nanoid()}>
-                    <h2>{quiz.question}</h2>
-                    <ul>
-                        {quiz.answers.map(answer=>(<li key={nanoid()}>{answer}</li>))}
-                    </ul>
-                </div>
-
-            )})
-            setQuiz(rendered_quizzes)
+            setQuiz(quizData)
 
         })
         .catch(e=>console.log("error ", e))
@@ -49,12 +139,17 @@ export default function App() {
 
     return (
         <div className="main">
-            <div style={styles} className="overlay">
+            <div style={overlayStyles} className="overlay">
                 <h1>Quizzical</h1>
                 <h6>Some description if needed</h6>
                 <button  onClick={hideOverlay}>Start quiz</button>
             </div>
-            <div className="mainQuiz">{quizzes}</div>
+            <div className="mainQuiz" style={quizzesStyles}>
+                {quizzes.map((quiz, i)=>(<Quiz quiz={quiz} index={i} graded={graded} onClickAnswer={handleAnswerClick}/>))}
+                <button onClick={toggleButton}>{!graded? "Check answers": "How did you do? Restart quiz"}</button>
+            </div>
+            
         </div>
     )
 }
+
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -3,10 +3,98 @@ import ReactDOM from "react-dom"
 
 
 
-export default function Quiz() {
+export default function Quiz({quiz, index, graded, onClickAnswer, }) {
+   //quiz is the quiz object, an array element of quiz set, onClickAnswer is the function that modifies quiz object attributes
+    function setAnswerStyles(clicked, isCorrect, graded){
+            const defaultStyles = {color: "black"}
+            if(graded) {
+                if (clicked&&isCorrect) {
+                    return {...defaultStyles, color:"green"}
+                }
+                else if(clicked){
+                    return {...defaultStyles, color: "red"}
+                }
+                else if (isCorrect){
+                    return {...defaultStyles, color: "green"}
     
+                }
+                else {
+                    return {...defaultStyles, color: "black"}
+                }
+            }
+            else {
+                return clicked?{...defaultStyles, color:"purple"}:{...defaultStyles, color:"black"}
+            }
+        }
+    // const {question, answers, graded} = quiz; 
+    // console.log('rendered', quiz)
     return (     
-        <div>This is quiz</div>
+        <div key={`q-${index}`}>
+            <h2>{quiz.question}</h2>
+            <ul>
+                {
+                quiz.answers.map((answer, j)=>{
+                    return (
+                    <li 
+                        key={`q-${index}-a-${j}`} 
+                        onClick={()=>{onClickAnswer(index,j)}}
+                        style={setAnswerStyles(answer.clicked, answer.isCorrect, graded)}
+                        >
+                        {answer.data}
+                    </li>)}) 
+                }
+            </ul>
+        </div>
  
     )
 }
+
+
+ // function renderedQuizzes(quizArray){
+    //     return quizArray.map((quiz, i)=>{
+    //         const temp = (
+    //         <div key={`q-${i}`}>
+    //             <h2>{quiz.question}</h2>
+    //             <ul>
+    //                 {
+    //                 quiz.answers.map((answer, j)=>{
+                        
+    //                     return (
+    //                     <li 
+    //                         key={`q-${i}-a-${j}`} 
+    //                         onClick={()=>{answerChosen(i,j)}}
+    //                         style={setAnswerStyles(answer.clicked, answer.isCorrect, graded)}
+    //                         >
+    //                         {answer.data}
+    //                     </li>)}) 
+    //                 }
+    //             </ul>
+    //         </div>
+    //         )
+    //         return temp
+    //     })
+    // }
+
+      // function setAnswerStyles(clicked, correct, graded){
+    //     const defaultStyles = {color: "black"}
+    //     if(graded) {
+
+                        
+    //         if (clicked&&correct) {
+    //             return {...defaultStyles, color:"green"}
+    //         }
+    //         else if(clicked){
+    //             return {...defaultStyles, color: "red"}
+    //         }
+    //         else if (correct){
+    //             return {...defaultStyles, color: "green"}
+
+    //         }
+    //         else {
+    //             return {...defaultStyles, color: "black"}
+    //         }
+    //     }
+    //     else {
+    //         return clicked?{...defaultStyles, color:"purple"}:{...defaultStyles, color:"black"}
+    //     }
+    // }
